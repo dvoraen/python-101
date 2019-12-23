5ed0c6219e455d2ea929da16fdde06efb6d91f83
dvoraen <42825596+dvoraen@users.noreply.github.com>
2019-12-23 09:24:15 -0800

    PracticePython - Exercise 1

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+#exercise1.py
+
+
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+age = int(age)
+
+print("Your name is %s and you will be 100 years old in the year %d." % (name, (2019 + 100 - age)))
