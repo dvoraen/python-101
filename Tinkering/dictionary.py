1f54920ff4113efc1192f7bfef3cabc0f4345a1b
dvoraen <42825596+dvoraen@users.noreply.github.com>
2020-04-19 07:30:28 -0700

    Dictionary note

--- Tinkering/dictionary.py
+++ Tinkering/dictionary.py
@@ -1,17 +1,14 @@
+dict1 = {"hi":1, "hello":2, "how goes?":3}
+dict2 = {"me":1, "myself":2, "Irene":3}
 
 
 def func(data:dict = {}) -> int:
     fdict = data
 
+    # This is what I want: the paired data.
     for value in fdict:
-        print(value[0])
+        print(fdict[value])
 
-    for value in fdict:
-        print(value[1])
-
-
-dict1 = {"hi":1, "hello":2, "how goes?":3}
-dict2 = {"me":1, "myself":2, "Irene":3}
 
 
 func()
